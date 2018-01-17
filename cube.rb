a3f954e3d89f8f93527a7aae372e6a69a4623295
Juliana Davila R <judaro13@gmail.com>
2018-01-17 11:31:05 -0500

    adding validation method for input point

--- cube.rb
+++ cube.rb
@@ -1,19 +1,28 @@
 class Cube
   attr_reader :matrix_size, :matrix
   def initialize(matrix_size:)
+    return false if matrix_size > 100
     @matrix_size = matrix_size
     coord = [0]*matrix_size
-    @matrix = [coord, coord, coord]
+    @matrix = [[coord]*matrix_size]*matrix_size
   end
 
   def matrix
     @matrix
   end
 
-  def update(x:, y:, z:, value:)
+  # point {x: INT, y: INT, z: INT }
+  def update(point:, value:)
+    return false unless valid_point?(point)
   end
 
   def query(point1:, point2:)
+    return false unless valid_point?(point1) || valid_point?(point2)
+  end
+
+  def valid_point?(point:)
+    ([:x, :y, :z] - point.keys).empty? &&
+    !point.values.any?{|x| !x.integer? || x > matrix_size || x < 0 }
   end
 
 end
