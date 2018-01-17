5ab7ddc60c815ac115138dbb32502680492f0e29
Juliana Davila R <judaro13@gmail.com>
2018-01-17 14:41:03 -0500

    added function to reduce matrix and sum with given points

--- cube.rb
+++ cube.rb
@@ -1,10 +1,10 @@
 class Cube
   attr_reader :matrix_size, :matrix
+
   def initialize(matrix_size:)
     raise "matrix size must be between 1 and 100" if matrix_size > 100 || matrix_size < 1
-    @matrix_size = matrix_size
-    coord = [0]*matrix_size
-    @matrix = [[coord]*matrix_size]*matrix_size
+    @matrix_size = matrix_size-1
+    @matrix = Array.new(matrix_size){ Array.new(matrix_size) { Array.new(matrix_size, 0) }}
   end
 
   def matrix
@@ -13,16 +13,65 @@ class Cube
 
   # point {x: INT, y: INT, z: INT }
   def update(point:, value:)
-    return false unless valid_point?(point)
+    return false unless valid_point?(point) || !(value.kind_of? Integer)
+    @matrix[point[:z]][point[:x]][point[:y]] = value
   end
 
   def query(point1:, point2:)
-    return false unless valid_point?(point1) || valid_point?(point2)
+    return false unless valid_point_intersection?(point1, point2)
+    q_matrix = reduce_matrix(point1, point2)
+    q_matrix.flatten.inject(0){|sum,x| sum + x }
   end
 
-  def valid_point?(point:)
+  def reduce_matrix(point1, point2)
+    q_matrix = @matrix.clone
+    x_remove = positions_for_remove(point1[:x], point2[:x])
+    y_remove = positions_for_remove(point1[:y], point2[:y])
+    z_remove = positions_for_remove(point1[:z], point2[:z])
+
+    z_remove.each_with_index do |z, i|
+      q_matrix.delete_at(z-i)
+    end
+
+    q_matrix.each_with_index do |z, zi|
+      x_remove.each_with_index do |x, xi|
+        q_matrix[zi].delete_at(x-xi)
+      end
+    end
+
+    q_matrix.each_with_index do |z, zi|
+      z.each do |x, xi|
+        y_remove.each_with_index do |y, yi|
+          q_matrix[zi][xi].delete_at(y-yi)
+        end
+      end
+    end
+
+    q_matrix
+
+  end
+
+  def positions_for_remove(coord1, coord2)
+    [*0..@matrix_size] - ([*coord1..coord2]||[*0..@matrix_size])
+  end
+
+  def valid_point_intersection?(point1, point2)
+    return false unless valid_point?(point1) || valid_point?(point2)
+    point1[:x] <= point2[:x] && point1[:y] <= point2[:y] && point1[:z] <= point2[:z]
+  end
+
+  def valid_point?(point)
     ([:x, :y, :z] - point.keys).empty? &&
-    !point.values.any?{|x| !x.integer? || x > matrix_size || x < 0 }
+    !point.values.any?{|x| !(x.kind_of? Integer) || x > @matrix_size || x < 0 }
   end
 
 end
+
+
+# 
+# require "pry"
+#
+# binding.pry
+
+# point1 = {x: 1, y: 4, z: 2 }
+# point2 = {x: 5, y: 4, z: 3 }
--- tests/cube_test.rb
+++ tests/cube_test.rb
@@ -14,27 +14,41 @@ class  CubeTest < Minitest::Test
     point = {x: 0, y: 1, z: 1}
 
     v_point = cube.valid_point?(point)
-    asset v_point
+    assert v_point
 
     point = {x: 0, y: 1, z: 2}
     v_point = cube.valid_point?(point)
-    asset v_point == false
+    assert v_point == false
 
     point = {x: 0, y: 1, z: "2"}
     v_point = cube.valid_point?(point)
-    asset v_point == false
+    assert v_point == false
 
     point = {y: 1, z: "2"}
     v_point = cube.valid_point?(point)
-    asset v_point == false
+    assert v_point == false
   end
 
   def test_update_point
     cube = Cube.new(matrix_size: 2)
     point = {x: 0, y: 1, z: 1}
     cube.update(point: point, value: 5)
-    asset cube.matrix[point[:z]][point[:x]][point[:y]] == 5
+    assert cube.matrix[point[:z]][point[:x]][point[:y]] == 5
+    flat = cube.matrix.flatten
+    result = flat.each_with_object(Hash.new(0)) { |k,counts| counts[k] += 1 }
+    assert result[5] == 1
   end
 
+  def test_valid_point_intersection
+    cube = Cube.new(matrix_size: 4)
+    point1 = {x: 0, y: 1, z: 1}
+    point2 = {x: 0, y: 2, z: 3}
+
+    assert cube.valid_point_intersection?(point1, point2)
+
+    point2 = {x: 0, y: 0, z: 3}
+    assert cube.valid_point_intersection?(point1, point2) == false
+
+  end
 
 end
